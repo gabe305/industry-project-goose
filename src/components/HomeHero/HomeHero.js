1e0aaab79d441891eeda42f0087fb4f3ec0efde3
gabe305 <gabecruz718@gmail.com>
2022-06-24 14:13:35 -0400

    minor changes ux wanted

--- src/components/HomeHero/HomeHero.js
+++ src/components/HomeHero/HomeHero.js
@@ -2,18 +2,21 @@ import spring from '../../assets/Images/spring.png'
 import winter from '../../assets/Images/winter.png'
 import './HomeHero.scss'
 import heroText from '../../assets/Images/hero-text.png'
+import winterBtn from '../../assets/Images/winter-btn.png'
+
 
 function HomeHero({ isSpring }) {
     if(isSpring) {
         return ( 
-            <section className="hero hero--spring">
+            <div className="hero hero__spring">
                 <img src={heroText} />
-            </section>
+            </div>
         )
     } else {
         return(
             <>
-                <div className="hero hero--winter">
+                <div className="hero hero__winter">
+                    <img className="hero__winter-btn" src={winterBtn} />
                 </div>            
             </>
         )
